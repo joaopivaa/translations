2bbae2a752e246d1d1aa4ea2970a0d4b84735495
idosonaweb <joao.piva@aluno.ifsp.edu.br>
2023-05-19 14:55:21 -0300

    commit

--- formulario.php
+++ formulario.php
@@ -20,27 +20,49 @@
             <h2>Translations</h2>
 
             <div class="inputBox">
-
                 <input type="text" id="palavra" name="palavra" size="28" required>
                 <label for="palavra">Palavra</label>
-
-<<<<<<< HEAD
                 <i></i>
-=======
-                <div>
-                    <input type="submit" value="Adicionar">
-                </div>
-
->>>>>>> ed6a5cdf7ad6a9c99c53330f21b51b0a95b1dbf2
             </div>
             <div class="inputBox">
-
                 <input type="text" id="traducao" name="traducao" size="28" required>
                 <label for="traducao">Tradução</label>
-
                 <i></i>
             </div>
-            <br><br>
+            <div class="inputBox">
+                <select name="letra" id="letra" required>
+                    <option value="1">Qual a letra?</option>
+                    <option value="2">A</option>
+                    <option value="3">B</option>
+                    <option value="4">C</option>
+                    <option value="5">D</option>
+                    <option value="6">E</option>
+                    <option value="7">F</option>
+                    <option value="8">G</option>
+                    <option value="9">H</option>
+                    <option value="10">I</option>
+                    <option value="11">J</option>
+                    <option value="12">K</option>
+                    <option value="13">L</option>
+                    <option value="14">M</option>
+                    <option value="15">N</option>
+                    <option value="16">O</option>
+                    <option value="17">P</option>
+                    <option value="18">Q</option>
+                    <option value="19">R</option>
+                    <option value="20">S</option>
+                    <option value="21">T</option>
+                    <option value="22">U</option>
+                    <option value="23">V</option>
+                    <option value="24">W</option>
+                    <option value="25">X</option>
+                    <option value="26">Y</option>
+                    <option value="27">Z</option>
+                    <option value="28">CC</option>
+                </select>
+                <i></i>
+            </div>
+            <br>
             <div class="container-btn">
                 <input type="submit" value="Adicionar">
             </div>
