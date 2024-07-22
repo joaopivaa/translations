4b213d873d918726916dfc3e5d0d3257058013c7
joaopivaa <joao.piva@aluno.ifsp.edu.br>
2024-07-22 12:59:32 -0300

    commit

--- formulario.php
+++ formulario.php
@@ -1,20 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link rel="stylesheet" href="lib/css/formulario.css">
-
     <title>Translations</title>
 </head>
-
 <body>
-
     <div class="box">
-
         <div class="form">
 
             <form method="POST" action="core/word_repositorio.php">
